cd9631e8296da0a5dba33fc623d22c745fa56bc8
Renata <127457027+RenataSch@users.noreply.github.com>
2023-10-18 08:01:17 -0300

    Add files via upload

---
+++ Aula4/Botão/script.js
@@ -0,0 +1,11 @@
+function doDemo(button){
+var square = document.getElementById("square");
+square.style.backgroundColor = "#fa4";
+button.setAttribute("disabled" , "true");
+setTimeout(clearDemo, 2000, button);
+}
+function clearDemo(button){
+    var square = document.getElementById("square");
+    square.style.background = "transparent";
+    button.removeAttribute("disable");
+}
